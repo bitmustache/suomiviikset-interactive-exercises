2703edf5dc2ea649d964275ecc25768a6e675761
bitmustache <dmg4ming@gmail.com>
2026-04-12 00:01:44 +0300

    testing

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "finnmustache",
+    pageTitle: "🇫🇮",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,

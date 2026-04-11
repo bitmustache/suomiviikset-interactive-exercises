6a2f4aae8909c96fb46ec7664972dd6cf4678ebb
bitmustache <dmg4ming@gmail.com>
2026-04-11 23:48:20 +0300

    some testing

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "suomiviikset – interactive exercises",
+    pageTitle: "finnmustache",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,

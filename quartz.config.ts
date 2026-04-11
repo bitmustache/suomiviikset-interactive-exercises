6230185bc66bc7edae7e1d596d321ecf75152de8
bitmustache <dmg4ming@gmail.com>
2026-04-11 22:05:37 +0300

    config

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "bitmustache.github.io/suomiviikset-interactive-exercises",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {

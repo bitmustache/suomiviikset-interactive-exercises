1bc5d2da6775dc1c8c0d43d3314837ead0e3f1e0
bitmustache <dmg4ming@gmail.com>
2026-07-18 23:48:01 +0300

    audio-quiz to load after page loads

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [Component.Flashcards(), Component.Tarkista(),],
+  afterBody: [Component.Flashcards(), Component.Tarkista(), () => `<script src="/static/audio-quiz.js" defer></script>`],
   footer: Component.Footer({
     links: {
     },
